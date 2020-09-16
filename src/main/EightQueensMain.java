6340a6e02e9474f12de607b1ece78d59fefa6b30
Brian-Thomas Rogers <btcr1990@gmail.com>
2020-09-15 20:38:06 -0500

    Print board method defined

--- src/main/EightQueensMain.java
+++ src/main/EightQueensMain.java
@@ -3,7 +3,15 @@ package main;
 public class EightQueensMain {
 
 	public static void printBoard(char board[][]) {
-		
+		for(int rank = 0; rank < board.length; rank++) {
+			for(int file = 0; file < board[rank].length; file++) {
+				System.out.print("[");
+				char piece = board[rank][file] == '\0' ? ' ' : board[rank][file];
+				System.out.print(piece);
+				System.out.print("]");
+			}
+			System.out.println();
+		}
 	}
 	
 	public static void main(String[] args) {
