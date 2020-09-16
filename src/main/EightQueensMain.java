1f75b8cab66c09457561994949b3f81c4406e453
Brian-Thomas Rogers <btcr1990@gmail.com>
2020-09-16 01:09:28 -0500

    Bruteforce solution to the eight queens problem.

--- src/main/EightQueensMain.java
+++ src/main/EightQueensMain.java
@@ -1,9 +1,12 @@
 package main;
 
+
 public class EightQueensMain {
 
 	public static void printBoard(char board[][]) {
+		System.out.println("   A  B  C  D  E  F  G  H");
 		for(int row = 0; row < board.length; row++) {
+			System.out.print((board.length - row) + " ");
 			for(int col = 0; col < board[row].length; col++) {
 				System.out.print("[");
 				char piece = board[row][col] == '\0' ? ' ' : board[row][col];
@@ -18,7 +21,7 @@ public class EightQueensMain {
 		board[row][col] = 'Q';
 	}
 	
-	public static void clearSpace(char board[][], int row, int col) {
+	public static void removeQueen(char board[][], int row, int col) {
 		board[row][col] = '\0';
 	}
 	
@@ -26,7 +29,6 @@ public class EightQueensMain {
 		//horizontal check
 		for(int c = 0; c != col; c++) {
 			if(board[row][c] == 'Q' && c != col) {
-				System.out.println("Horizontal Check");
 				return true;
 			}
 		}
@@ -50,17 +52,33 @@ public class EightQueensMain {
 		return false;
 	}
 	
-	public static void solveEightQueens(char board[][]) {
+	public static boolean solveEightQueens(char board[][], int col) {
+		if(col >= board[0].length) {
+			return true;
+		}
 		
+		boolean solved = false;
+		for(int row = 0; row < board.length; row++) {
+			if(isAttacking(board, row, col)) {
+				continue;
+			}
+			placeQueen(board, row, col);
+			solved = solveEightQueens(board, col+1);
+			if(!solved) {
+				removeQueen(board, row, col);
+			} else {
+				break;
+			}
+		}
+		
+		return solved;
 	}
 	
 	public static void main(String[] args) {
 		char board[][] = new char[8][8];
-		
-		placeQueen(board, 3, 5);
-		placeQueen(board, 0, 1);
+		solveEightQueens(board, 0);
+		System.out.println();
 		printBoard(board);
-		System.out.println(isAttacking(board, 3, 5));
 	}
 
 }
