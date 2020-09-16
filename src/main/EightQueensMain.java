d16897997e1762dbd73784800587399799a7e597
Brian-Thomas Rogers <btcr1990@gmail.com>
2020-09-15 23:48:00 -0500

    Check for a queen attacking a queen left of the board
    Since we are going from left to right and placing the queens then
    there is no reason to check if the queen is attacking another queen
    to the right.  Furthermore, check on the vertical is not necessary
    since we will be placing a single queen on the files of the board.

--- src/main/EightQueensMain.java
+++ src/main/EightQueensMain.java
@@ -3,10 +3,10 @@ package main;
 public class EightQueensMain {
 
 	public static void printBoard(char board[][]) {
-		for(int file = 0; file < board.length; file++) {
-			for(int rank = 0; rank < board[file].length; rank++) {
+		for(int row = 0; row < board.length; row++) {
+			for(int col = 0; col < board[row].length; col++) {
 				System.out.print("[");
-				char piece = board[file][rank] == '\0' ? ' ' : board[file][rank];
+				char piece = board[row][col] == '\0' ? ' ' : board[row][col];
 				System.out.print(piece);
 				System.out.print("]");
 			}
@@ -14,16 +14,40 @@ public class EightQueensMain {
 		}
 	}
 	
-	public static void placeQueen(char board[][], int file, int rank) {
-		board[file][rank] = 'Q';
+	public static void placeQueen(char board[][], int row, int col) {
+		board[row][col] = 'Q';
 	}
 	
-	public static void clearSpace(char board[][], int file, int rank) {
-		board[file][rank] = '\0';
+	public static void clearSpace(char board[][], int row, int col) {
+		board[row][col] = '\0';
 	}
 	
-	public static void checkIfAttacking(char board[][], int file, int rank) {
+	public static boolean isAttacking(char board[][], int row, int col) {
+		//horizontal check
+		for(int c = 0; c != col; c++) {
+			if(board[row][c] == 'Q' && c != col) {
+				System.out.println("Horizontal Check");
+				return true;
+			}
+		}
 		
+		//diagonal checks
+		
+		//top left diagonal
+		for(int r = row - 1, c = col - 1; r >= 0 && c >= 0; r--, c--) {
+			if(board[r][c] == 'Q') {
+				return true;
+			}
+		}
+		
+		//bottom left diagonal
+		for(int r = row + 1, c = col - 1; r < board.length && c >= 0; r++, c--) {
+			if(board[r][c] == 'Q') {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 	
 	public static void solveEightQueens(char board[][]) {
@@ -33,7 +57,10 @@ public class EightQueensMain {
 	public static void main(String[] args) {
 		char board[][] = new char[8][8];
 		
+		placeQueen(board, 3, 5);
+		placeQueen(board, 0, 1);
 		printBoard(board);
+		System.out.println(isAttacking(board, 3, 5));
 	}
 
 }
