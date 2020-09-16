12ff7c0d8f6984672f1459045e5e00fc275ec3bd
Brian-Thomas Rogers <btcr1990@gmail.com>
2020-09-15 20:35:52 -0500

    Initial Main Method

---
+++ src/main/EightQueensMain.java
@@ -0,0 +1,15 @@
+package main;
+
+public class EightQueensMain {
+
+	public static void printBoard(char board[][]) {
+		
+	}
+	
+	public static void main(String[] args) {
+		char board[][] = new char[8][8];
+		
+		printBoard(board);
+	}
+
+}
